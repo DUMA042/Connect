ea7c49444d386ad8aa2f4f0a281882da19be49bc
DUMA042 <dumebiikeoffiah@gmail.com>
2023-09-12 02:01:58 +0300

    Wordy_Branch branch commit

--- app/src/main/java/com/example/connect/MainActivity.kt
+++ app/src/main/java/com/example/connect/MainActivity.kt
@@ -4,19 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.connect.Viewmodel_packages.MarsViewModel
 import com.example.connect.ui.theme.ConnectTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -46,33 +63,47 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     val marsViewModel = viewModel(modelClass = MarsViewModel::class.java)
   val lands by marsViewModel.lands.collectAsState()
 
+
 Column {
 LazyColumn{
-    items(lands){item ->
+    items(lands){land ->
 
-        Text(
-            text = "--------The Lands: ${item.type}!",
-            modifier = modifier
-        )
+       displayLandsDitails(marLands = land,modifier)
     }
 }
 
-    Text(
-        text = "--------The Lands::::: ${lands}!",
-        modifier = modifier
-    )
-    Button(onClick = {marsViewModel.getlands()}) {
-        Text(
-            text = "Reload",
-            modifier = modifier
-        )
-    }
 
-    Text(
-        text = "--------Hello $name!",
-        modifier = modifier
-    )
 }
+
+
+}
+
+@Composable
+fun displayLandsDitails(marLands: MarLands,modifier: Modifier) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(marLands.imgsrc)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Mars Land Images",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(80.dp)
+            )
+           Column {
+               Text(marLands.type)
+               Text(marLands.price.toString())
+           }
+
+        }
+    }
 
 
 }
